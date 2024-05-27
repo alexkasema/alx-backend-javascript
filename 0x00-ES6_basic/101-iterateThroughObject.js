0abc11eba09aec074c7024565ce7bf80b7dcb3cc
Alex Kasema <lexxmulwa@gmail.com>
2024-05-27 09:14:31 -0700

    Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+  for (const name of reportWithIterator) {
+    employees.push(name);
+  }
+
+  return employees.join(' | ');
+}
---
+++ 0x00-ES6_basic/101-main.js
@@ -0,0 +1,5 @@
+import iterateThroughObject from './101-iterateThroughObject';
+
+const names = ['Bob', 'Ghost', 'Lobos']
+
+console.log(iterateThroughObject(names));
