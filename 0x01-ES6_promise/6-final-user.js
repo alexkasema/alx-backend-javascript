d643e2d6c621ec5914d5db8fed65da728ec8f7bf
Alex Kasema <lexxmulwa@gmail.com>
2024-05-29 01:30:30 -0700

    Handle multiple promises, getting the result of their settled promises. Make the function async..

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((value) => {
+    .then((value) => (
       value.map((val) => ({
         status: val.status,
         value: val.status === 'fulfilled' ? val.value : String(val.reason),
-      }));
-    });
+      }))
+    ));
 }
