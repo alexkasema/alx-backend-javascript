8b209f65a186bb0ed52be6c03653f96b8957b525
Alex Kasema <lexxmulwa@gmail.com>
2024-07-22 13:16:52 +0000

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const { readFileSync } = require('fs');
+
+const countStudents = (path) => {
+  let data;
+  try {
+    data = readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  data = data.toString().split('\n');
+
+  let students = data.filter((item) => item);
+  students = students.map((item) => item.split(','));
+
+  const numberOfStudents = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${numberOfStudents}`);
+  const fields = {};
+
+  for (const i in students) {
+    if (i !== 0) {
+      if (!fields[students[i][3]]) fields[students[i][3]] = [];
+      fields[students[i][3]].push(students[i][0]);
+    }
+  }
+
+  delete fields.field;
+
+  for (const field of Object.keys(fields)) {
+    console.log(
+      `Number of students in ${field}: ${fields[field].length}. List: ${fields[
+        field
+      ].join(', ')}`
+    );
+  }
+};
+
+module.exports = countStudents;
