18f4d594da5226a328267102f4d301c95d2a1d8e
Alex Kasema <lexxmulwa@gmail.com>
2024-07-24 15:40:14 +0000

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,28 +3,23 @@ const calculateNumber = require('./0-calcul');
 
 describe('Test calculateNumber function:', function () {
   describe('The sum of two integers:', function () {
-    it('Should return 7.', function () {
-      assert.strictEqual(calculateNumber(4, 3), 7);
+    it('Should return 4.', function () {
+      assert.strictEqual(calculateNumber(1, 3), 4);
     });
   });
   describe('The sum of a float and an integer (round up):', function () {
     it('Should return 5.', function () {
-      assert.strictEqual(calculateNumber(2.7, 2), 5);
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
   });
   describe('The sum of a float and an integer (round down):', function () {
-    it('Should return 10.', function () {
-      assert.strictEqual(calculateNumber(5.4, 5), 10);
-    });
-  });
-  describe('The sum of two floats:', function () {
-    it('Should return 8.', function () {
-      assert.strictEqual(calculateNumber(4.3, 3.8), 8);
+    it('Should return 5.', function () {
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
   });
   describe('The sum of two floats with boarderline:', function () {
-    it('Should return 3.', function () {
-      assert.strictEqual(calculateNumber(1.5, 1.4), 3);
+    it('Should return 6.', function () {
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
   });
 });
